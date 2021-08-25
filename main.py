313a1ba43d5d96d1d6be8c384640615a13ea62e4
riley <invalidfire@fastmail.com>
2021-08-24 22:56:16 -0400

    Change listening address

--- main.py
+++ main.py
@@ -2,5 +2,8 @@ import sys
 
 from core.wrapper import Wrapper
 
-api = Wrapper(['gunicorn', "-k", "uvicorn.workers.UvicornWorker", "--log-config", "log.conf", 'api:api'], sys.stdout)
+api = Wrapper(['gunicorn', "-k", "uvicorn.workers.UvicornWorker",
+               "--log-config", "log.conf",
+               '--bind', '0.0.0.0:8000',
+               'api:api'], sys.stdout)
 api.start()
