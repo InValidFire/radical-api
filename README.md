ffef9b6f07318529b9f764398685ad546d94e00b
Riley Housden <invalidfire@fastmail.com>
2021-11-30 03:11:43 -0500

    Require python3.9

---
+++ README.md
@@ -0,0 +1,5 @@
+# Radical API
+
+Requires Python 3.9
+
+I use this API for a variety of things, really it's just a collection of random stuff I add.
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 
 from core.wrapper import Wrapper
 
-api = Wrapper(['python3', '-m', 'gunicorn', "-k", "uvicorn.workers.UvicornWorker",
+api = Wrapper(['python3.9', '-m', 'gunicorn', "-k", "uvicorn.workers.UvicornWorker",
                "--log-config", "log.conf",
                '--bind', '0.0.0.0:8000',
                'api:api'], sys.stdout)
