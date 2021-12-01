16b3d03841bd624f973664c75e9465462e81d030
Riley Housden <invalidfire@fastmail.com>
2021-12-01 01:52:14 -0500

    Remove wrapper - Removed wrapper.py - Removed main.py - modified radical-api.service to launch the gunicorn process directly. - Removed exit code in update method.

--- main.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-from core.wrapper import Wrapper
-
-api = Wrapper(['python3.9', '-m', 'gunicorn', "-k", "uvicorn.workers.UvicornWorker",
-               "--log-config", "log.conf",
-               '--bind', '0.0.0.0:8000',
-               'api:api'], sys.stdout)
-api.start()
--- routers/system.py
+++ routers/system.py
@@ -12,4 +12,4 @@ router = APIRouter()
 @router.post("/system/update")
 def update(access_token: APIKey = Depends(get_system_key)):
     output = subprocess.check_output(['git', 'pull'], encoding='utf-8')
-    sys.exit(26)  # exit with restart code
+    sys.exit()
